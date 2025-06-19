bdba0872f24cb90757c5c8a02082cb869d8ee2ef
patel-niraj-2004 <216977539+niraj-patel-9099@users.noreply.github.com>
2025-06-19 07:02:12 +0530

    Create 02_count_occurances_of_substring_of_string.py

---
+++ Assignment/Prectical Task/02_count_occurances_of_substring_of_string.py
@@ -0,0 +1,5 @@
+string=input("Enter the string :")
+sub=input("entre the sub stirng :")
+
+count=string.count(sub) # Used .count the occuracances of sub sting in string.
+print(f"the substirng apears {count} times in the string")
